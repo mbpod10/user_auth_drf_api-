659015b5cb9c05d67fbea623d8bef7f9571febc2
M. Brock Podgurski <mbpod10@gmail.com>
2022-03-03 11:03:56 -0500

    finish automated tests

--- app/profiles/tests/test_profiles.py
+++ app/profiles/tests/test_profiles.py
@@ -1,5 +1,6 @@
 import json
 
+
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
@@ -13,6 +14,8 @@ from rest_framework.test import APIClient
 REGISTER_URL = '/api/rest-auth/regisration/'
 PROFILES_URL = reverse("profile-list")
 PROFILE_DETAIL_URL = reverse("profile-detail", kwargs={"pk": 1})
+
+STATUS_LIST_URL = reverse('status-list')
 
 
 class RegistrationTestCase(TestCase):
@@ -78,12 +81,11 @@ class ProfileTestCase(TestCase):
         """test if user can update"""
         response = self.client.put(PROFILE_DETAIL_URL, {
             "city": 'Florence', 'bio': 'Ren Genius'})
-        print(response.data)
+        # print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(json.loads(response.content), {
                          'id': 1, 'user': 'davinci', 'bio': 'Ren Genius', 'city': 'Florence',
                          'avatar': None})
-        # self.assertIn('user', response.data)
 
     def test_update_user_by_other_user(self):
         """test if random user cant update"""
@@ -96,3 +98,61 @@ class ProfileTestCase(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertNotIn('florence', response.data)
+
+
+class ProfileStatusTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            username="davinci", password="Sometnewpass123%")
+        self.status = ProfileStatus.objects.create(
+            user_profile=self.user.profile, status_content='test status')
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        """ authenticate set up"""
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+
+    def test_authenticated_user_profile_status_list(self):
+        """ test authenticated user can reveiece profile_list data"""
+        response = self.client.get(STATUS_LIST_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_unauthenticated_user_profile_status_list(self):
+        """ test unauthenticated user cannot reveiece profile_list data"""
+        self.client.force_authenticate(user=None)
+        response = self.client.get(STATUS_LIST_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_status_create(self):
+        """ test create a new status"""
+        payload = {'status_content': 'a new status!'}
+        response = self.client.post(STATUS_LIST_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['user_profile'], 'davinci')
+        self.assertEqual(response.data['status_content'], 'a new status!')
+
+    def test_single_status_retrieve(self):
+        """test to see if current user can retrieve status"""
+        serializer_data = ProfileStatusSerializer(instance=self.status).data
+        response = self.client.get(reverse('status-detail', kwargs={'pk': 1}))
+        response_data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer_data, response_data)
+
+    def test_status_update_owner(self):
+        """ test updated a status status"""
+        payload = {'status_content': 'a new status updated!'}
+        response = self.client.put(
+            reverse('status-detail', kwargs={'pk': 1}), payload)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['user_profile'], 'davinci')
+        self.assertEqual(
+            response.data['status_content'], 'a new status updated!')
